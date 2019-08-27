f8a671688e6bc08789fb797aae1d828d42449780
Graham <greysnaa@gmail.com>
2019-08-27 16:24:04 -0500

    added expense_types and expenses routes blank files

---
+++ server/models/expenses.models.js
@@ -0,0 +1,37 @@
+const pool = require('../connections');
+
+function getIncome(res, userid) {
+    pool.query(`SELECT * FROM income WHERE user_id = ?`, userid, (err, results, field) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(results);
+        }
+    });
+}
+function addIncome(res, income) {
+    pool.query(`INSERT INTO income SET ?`, income, (err, results, field) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(results);
+        }
+    });
+}
+
+function deleteIncome(res, id) {
+    pool.query(`DELETE FROM income WHERE id = ?`, id, (err, results, field) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(results);
+        }
+    });
+}
+
+module.exports.addIncome = addIncome;
+module.exports.getIncome = getIncome;
+module.exports.deleteIncome = deleteIncome;
