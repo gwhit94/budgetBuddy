54c61accdc608448e442eed341e91baf3cd8f8e2
Graham <greysnaa@gmail.com>
2019-08-27 16:24:14 -0500

    added expense_types and expenses routes blank files

--- server/models/expenses.models.js
+++ server/models/expenses.models.js
@@ -1,37 +1,37 @@
-const pool = require('../connections');
+// const pool = require('../connections');
 
-function getIncome(res, userid) {
-    pool.query(`SELECT * FROM income WHERE user_id = ?`, userid, (err, results, field) => {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send(results);
-        }
-    });
-}
-function addIncome(res, income) {
-    pool.query(`INSERT INTO income SET ?`, income, (err, results, field) => {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send(results);
-        }
-    });
-}
+// function getExpense(res, userid) {
+//     pool.query(`SELECT * FROM  WHERE user_id = ?`, userid, (err, results, field) => {
+//         if (err) {
+//             res.send(err);
+//         }
+//         else {
+//             res.send(results);
+//         }
+//     });
+// }
+// function addExpense(res, income) {
+//     pool.query(`INSERT INTO income SET ?`, income, (err, results, field) => {
+//         if (err) {
+//             res.send(err);
+//         }
+//         else {
+//             res.send(results);
+//         }
+//     });
+// }
 
-function deleteIncome(res, id) {
-    pool.query(`DELETE FROM income WHERE id = ?`, id, (err, results, field) => {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send(results);
-        }
-    });
-}
+// function deleteExpense(res, id) {
+//     pool.query(`DELETE FROM income WHERE id = ?`, id, (err, results, field) => {
+//         if (err) {
+//             res.send(err);
+//         }
+//         else {
+//             res.send(results);
+//         }
+//     });
+// }
 
-module.exports.addIncome = addIncome;
-module.exports.getIncome = getIncome;
-module.exports.deleteIncome = deleteIncome;
+// module.exports.addExpense = addExpense;
+// module.exports.getExpense = getExpense;
+// module.exports.deleteExpense = deleteExpense;
