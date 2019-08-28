d3b01bed8ca212ab3c0a73d50f8c4046f97fe821
Graham <greysnaa@gmail.com>
2019-08-28 11:50:21 -0500

    Adds DB_PORT

--- server/connections.js
+++ server/connections.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
