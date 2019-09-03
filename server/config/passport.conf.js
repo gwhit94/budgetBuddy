ab1e28bfffc1c48d08fb2683e9c31cdff1944702
Graham <greysnaa@gmail.com>
2019-09-03 16:37:34 -0500

    added serialize and formatted

--- server/config/passport.conf.js
+++ server/config/passport.conf.js
@@ -5,20 +5,22 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const pool = require("../connections");
 
+passport.serializeUser((user, done) => done(null, user));
+
 passport.use("local-strat", new LocalStrategy((username, password, done) => {
-        pool.query(`SELECT * FROM user WHERE username = ?`, username, (err, results) => {
-            if (err){ 
-                return done(err);
-            }
-            if (!results[0]){ 
-                return done(null, false, { message: 'Incorrect Username.' });
-            }
-            if (!bcrypt.compareSync(password, results[0].password)){
-                return done(null, false, { message: 'Incorrect Password.' });
-            }
-            return done(null, results);
-        });
-    }
+    pool.query(`SELECT * FROM user WHERE username = ?`, username, (err, results) => {
+        if (err) {
+            return done(err);
+        }
+        if (!results[0]) {
+            return done(null, false, { message: 'Incorrect Username.' });
+        }
+        if (!bcrypt.compareSync(password, results[0].password)) {
+            return done(null, false, { message: 'Incorrect Password.' });
+        }
+        return done(null, { username: results[0].username, id: results[0].user_id, first_name: results[0].first });
+    });
+}
 ));
 // JWT 
 const opts = {}
