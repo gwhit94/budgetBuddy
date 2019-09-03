80c5eee8e4ad33941eadec8dc7c814bbe3941e9b
Graham <greysnaa@gmail.com>
2019-09-03 16:32:50 -0500

    passport jwt

--- server/config/passport.conf.js
+++ server/config/passport.conf.js
@@ -1,19 +1,40 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const pool = require("../connections");
 
-passport.use(new LocalStrategy(
-    function(username, password, done){
-        users.findOne({username: username}, function(err, user){
-            if (err){ return done(err);}
-            if (!user){ 
+passport.use("local-strat", new LocalStrategy((username, password, done) => {
+        pool.query(`SELECT * FROM user WHERE username = ?`, username, (err, results) => {
+            if (err){ 
+                return done(err);
+            }
+            if (!results[0]){ 
                 return done(null, false, { message: 'Incorrect Username.' });
             }
-            if (!user.validPassword(password)){
+            if (!bcrypt.compareSync(password, results[0].password)){
                 return done(null, false, { message: 'Incorrect Password.' });
             }
-            return done(null, user);
+            return done(null, results);
         });
     }
 ));
+// JWT 
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET_KEY;
+passport.use('jwt-strat', new JwtStrategy(opts, (jwt_payload, done) => {
+    pool.query(`SELECT * FROM user WHERE username = ?`, jwt_payload.username, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
+
+module.exports = passport;
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userFunctions = require('../models/users.models');
+// const passport = require('passport');
+// const jwt = require('jsonwebtoken');
 
 router.post('/login', (req, res) => {
+    // sign jwt with information about user
+    // let signedToken = jwt.sign({id: req.user.user_id}, process.env.SECRET_KEY);
+    // res.send({user: req.user, jwt: signedToken});
+
     userFunctions.getUser(res, req.body);
 })
 router.post('/signup', (req, res) => {
